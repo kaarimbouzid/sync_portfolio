87dde59209f4fc799cc608c8dd3f0d80ed9616a7
sabrinouira02 <sabri@sync.tn>
2024-09-23 10:52:03 +0100

    fly description2

--- src/app/home/portfolio-details/portfolio-details.component.ts
+++ src/app/home/portfolio-details/portfolio-details.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { PORTFOLIOS } from '../../shared/portfolio';
 import { Portfolio } from '../../shared/interfaces/portfolio';
 import { ProjectsService } from 'src/app/shared/services/projects.service';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portfolio-details',
@@ -14,13 +12,10 @@ export class PortfolioDetailsComponent implements OnInit {
   portfolios: Portfolio[] = PORTFOLIOS;
   projectName!: string;
   singleProject: any;
-  errorMessage: any;
   portfolio!: any;
   portfolioDetails!: any;
   allCategories: any;
   constructor(
-    private route: ActivatedRoute,
-    private translate: TranslateService,
     private projectService: ProjectsService
   ) {
     window.scroll({
@@ -31,22 +26,6 @@ export class PortfolioDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // Récupérer le nom slugifié depuis l'URL
-    // this.projectName = this.route.snapshot.paramMap.get('name') as string;
-
-    // // Rechercher le projet correspondant
-    // this.singleProject = this.portfolios.find((portfolio) => {
-    //   let translatedTitle = '';
-
-    //   // Traduire et slugifier le titre du projet
-    //   this.translate.get(portfolio.title).subscribe((translated: string) => {
-    //     translatedTitle = this.slugify(translated);
-    //   });
-
-    //   // Comparer le slug traduit avec le slug dans l'URL
-    //   return this.slugify(translatedTitle) === this.projectName;
-    // });
-
     const savedProject = localStorage.getItem('selectedProject');
 
   if (savedProject) {
@@ -56,14 +35,6 @@ export class PortfolioDetailsComponent implements OnInit {
     console.log('Aucun projet trouvé, redirection...');
     // Par exemple, vous pourriez rediriger l'utilisateur vers la liste des projets.
   }
-  }
-
-  // Fonction pour slugifier le texte (remplace les espaces et caractères spéciaux)
-  slugify(text: string): string {
-    return text
-      .toLowerCase()
-      .replace(/ /g, '-')        // Remplace les espaces par des tirets
-      .replace(/[^\w-]+/g, '');  // Enlève les caractères non alphanumériques
   }
 
   eliminerCaracteres(phrase: string) {
@@ -90,7 +61,6 @@ export class PortfolioDetailsComponent implements OnInit {
       },
       (error) => {
         // if any error, Code throws the error
-        this.errorMessage = error.error.message;
         console.log(error.error.message, 'error');
       }
     );
